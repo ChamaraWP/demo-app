6b465a59969c7d28dfc00253a9234d291ee827e1
chamarawp <pxdroid.cw@gmail.com>
2019-07-24 00:18:22 +0530

    form done

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  selector: "app-form",
+  templateUrl: "./form.component.html",
+  styleUrls: ["./form.component.css"]
 })
 export class FormComponent implements OnInit {
+  images = [
+    {
+      name: "1",
+      path: ".../../assets/1.jpg"
+    },
+    {
+      name: "2",
+      path: ".../../assets/2.jpg"
+    },
+    {
+      name: "3",
+      path: ".../../assets/3.jpg"
+    },
+    {
+      name: "4",
+      path: ".../../assets/4.jpg"
+    },
+    {
+      name: "5",
+      path: ".../../assets/5.jpg"
+    },
+    {
+      name: "6",
+      path: ".../../assets/6.jpg"
+    }
+  ];
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor() {}
+  // tslint:disable-next-line:align
+  ngOnInit() {}
 }
