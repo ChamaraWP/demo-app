e0721f5864a93b6c612033800b07d7ec01d23c0c
chamarawp <pxdroid.cw@gmail.com>
2019-07-23 18:36:12 +0530

    side-nav completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { SidemenuComponent } from "./components/sidemenu/sidemenu.component";
 import { FormComponent } from "./components/form/form.component";
+import { DoctorComponent } from './components/doctor/doctor.component';
 
 @NgModule({
-  declarations: [AppComponent, SidemenuComponent, FormComponent],
+  declarations: [AppComponent, SidemenuComponent, FormComponent, DoctorComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    RouterModule.forRoot([{ path: "", component: AppComponent }])
+    RouterModule.forRoot([{ path: '', component: FormComponent },
+    { path: 'doctor', component: DoctorComponent }])
   ],
+  exports: [RouterModule],
 
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
